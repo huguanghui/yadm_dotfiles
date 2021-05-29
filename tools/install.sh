f90ffad65fbb2c04d528bb4732103beeed0cff36
huguanghui <522146829@qq.com>
2021-05-30 07:42:41 +0800

    feat(tools): 添加软件安装脚本
    1.去掉环境相关的文件

---
+++ tools/install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -o nounset
+
+operate_type="ubuntu"
+
+[ "$(uname)" = "Dawin" ] && operate_type="mac"
+[ -n "$(cat /etc/os-release | grep "Ubuntu")" ] && operate_type="ubuntu"
+[ -f "/etc/arch-release" -o -f "/etc/artix-release" ] && operate_type="arch"
+
+echo "operate_type=${operate_type}"
+
+install_by_apt() {
+    echo "install by apt"
+    sudo apt install -y zsh tmux ranger lazygit silversearcher-ag
+}
+
+install_by_pacman() {
+    echo "install by pacman"
+}
+
+if [ "${operate_type}" = "ubuntu" ]; then
+    install_by_apt
+elif [ "${operate_type}" = "arch" ]; then
+    install_by_pacman
+else
+    echo "Unsupport operate"
+fi
