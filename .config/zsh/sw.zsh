9157d6605b3ff4da7a45a37b86acf5de7ed15b5c
hgh <522146829@qq.com>
2021-05-30 00:42:21 +0800

    fix(zsh) 支持powerlevel10k自动安装

--- .config/zsh/sw.zsh
+++ .config/zsh/sw.zsh
@@ -20,6 +20,10 @@ install_zsh_autosuggestions() {
   git clone git://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 }
 
+install_powerlevel10k() {
+  git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+}
+
 install_xmake() {
   bash <(curl -fsSL https://xmake.io/shget.text)
 }
@@ -28,8 +32,13 @@ install_xmake() {
 [ ! -d "$HOME/.oh-my-zsh" ] && install_oh_my_zsh
 
 # zsh_autosuggestions
-[ -d "$HOME/.oh-my-zsh/custom/plugins"  ] && \
-[ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"  ] && install_zsh_autosuggestions
+[ -d "$HOME/.oh-my-zsh/custom/plugins" ] && \
+[ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ] && install_zsh_autosuggestions
+
+# powerlevel10k 主题安装
+[ -d "$HOME/.oh-my-zsh/custom/themes" ] && \
+[ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ] && install_powerlevel10k
+
 
 # xmake 安装
 [ ! -d "$HOME/.xmake" ] && install_xmake
