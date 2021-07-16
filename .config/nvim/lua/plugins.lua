cda03b5c6d221e6bf4bb7acf7272780fff2815b2
huguanghui <522146829@qq.com>
2021-07-16 15:23:28 +0800

    feat(editorconfig) 编译操作规范

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -33,6 +33,7 @@ return require("packer").startup(function(use)
   use { "nvim-lua/popup.nvim" }
   use { "nvim-lua/plenary.nvim" }
   use { "tjdevries/astronauta.nvim" }
+  use { "editorconfig/editorconfig-vim" }
   use {
     "nvim-telescope/telescope.nvim",
     config = [[require('lv-telescope')]],
